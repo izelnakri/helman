55687d4dcb44ab91579c65e3f3913a626e704b3e
Izel Nakri <contact@izelnakri.com>
2020-03-27 23:33:22 +0100

    $ helman init should be ready now

--- commands/init.js
+++ commands/init.js
@@ -1,24 +1,102 @@
 import chalk from 'ansi-colors';
 import fs from 'fs-extra';
-import { promisify } from 'util';
-
-const shell = promisify(exec);
+import findProjectRoot from '../utils/find-project-root.js';
 
 export default async function () {
-  const projectRoot = await findProjectRoot('helm.json') || await findProjectRoot('package.json');
+  const projectRoot =
+    (await findProjectRoot('helm.json')) || (await findProjectRoot('package.json'));
 
-  if (!(await fs.exists(`${projectRoot}/helm.json`)) {
-    await fs.writeFile(`${projectRoot}/helm.json`, JSON.stringify({
-      name: projectRoot.slice(projectRoot.lastIndexOf('/') + 1),
-      dependencies: {}
-    }, null, 2));
+  if (!(await fs.exists(`${projectRoot}helm.json`))) {
+    await fs.writeFile(
+      `${projectRoot}helm.json`,
+      JSON.stringify(
+        {
+          name: projectRoot.slice(projectRoot.lastIndexOf('/') + 1),
+          dependencies: {},
+        },
+        null,
+        2
+      )
+    );
 
-    console.log(chalk.cyan(`created helm.json on ${projectRoot}/helm.json`));
+    console.log(chalk.cyan(`created helm.json on ${projectRoot}helm.json`));
   }
 
-  if (!(await fs.exists(`${projectRoot}/helm_charts`)) {
-    await fs.mkdirp(`${projectRoot}/helm_charts`);
+  if (!(await fs.exists(`${projectRoot}helm_charts`))) {
+    await fs.mkdirp(`${projectRoot}helm_charts`);
 
-    console.log(chalk.cyan(`created helm_charts folder on ${projectRoot}/helm_charts/`));
+    console.log(chalk.cyan(`created helm_charts folder on ${projectRoot}helm_charts/`));
   }
+
+  await fs.mkdirp(`${projectRoot}k8s`);
+  await fs.mkdirp(`${projectRoot}k8s/bases`);
+  await fs.mkdirp(`${projectRoot}k8s/prod/bases`);
+  await fs.mkdirp(`${projectRoot}k8s/staging/bases`);
+  await fs.mkdirp(`${projectRoot}k8s/test/bases`);
+
+  if (!(await fs.exists(`${projectRoot}k8s/bases/kustomization.yaml`))) {
+    await fs.writeFile(
+      `${projectRoot}k8s/bases/kustomization.yaml`,
+      `kind: Kustomization
+apiVersion: kustomize.config.k8s.io/v1beta1
+
+bases:
+
+resources:
+`
+    );
+
+    console.log(chalk.cyan('created ./k8s/bases/kustomization.yaml'));
+  }
+
+  if (!(await fs.exists(`${projectRoot}k8s/prod/bases/kustomization.yaml`))) {
+    await fs.writeFile(
+      `${projectRoot}k8s/prod/bases/kustomization.yaml`,
+      `kind: Kustomization
+apiVersion: kustomize.config.k8s.io/v1beta1
+
+bases:
+
+resources:
+`
+    );
+
+    console.log(chalk.cyan('created ./k8s/prod/bases/kustomization.yaml'));
+  }
+
+  if (!(await fs.exists(`${projectRoot}k8s/staging/bases/kustomization.yaml`))) {
+    await fs.writeFile(
+      `${projectRoot}k8s/staging/bases/kustomization.yaml`,
+      `kind: Kustomization
+apiVersion: kustomize.config.k8s.io/v1beta1
+
+bases:
+
+resources:
+`
+    );
+
+    console.log(chalk.cyan('created ./k8s/staging/bases/kustomization.yaml'));
+  }
+
+  if (!(await fs.exists('./k8s/test/bases/kustomization.yaml'))) {
+    await fs.writeFile(
+      `${projectRoot}k8s/test/bases/kustomization.yaml`,
+      `kind: Kustomization
+apiVersion: kustomize.config.k8s.io/v1beta1
+
+bases:
+
+resources:
+`
+    );
+
+    console.log(chalk.cyan('created ./k8s/test/bases/kustomization.yaml'));
+  }
+
+  console.log(chalk.green('helman init(helm <> kustomize setup) is complete.'));
+  console.log(
+    chalk.yellow('You can install helm charts locally and deploy them with kustomize. For example:')
+  );
+  console.log(chalk.yellow('helman install stable/nginx-ingress && kubectl apply -k ./k8s'));
 }
