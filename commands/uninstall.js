5f4eae9a9101afe35ef2b5b603c5d63554ca16c0
Izel Nakri <contact@izelnakri.com>
2020-04-02 04:48:28 +0200

    $ helman uninstall command complete with tests

--- commands/uninstall.js
+++ commands/uninstall.js
@@ -1,21 +1,51 @@
 import fs from 'fs-extra';
+import findProjectRoot from '../utils/find-project-root.js';
+import Console from '../utils/console.js';
+import YAML from 'yaml';
 
-export default function () {
-  //   if (!(await fs.pathExists(`${projectRoot}/helm.json`))) {
-  //   throw new Error('helm.json does not exists in this project. Did you run $ helm init first?');
-  // } else if (!(await fs.pathExists(`${projectRoot}/helm_charts`))) {
-  //   await setupHelmChartsFolder(projectRoot);
-}
+export default async function () {
+  let projectRoot = (await findProjectRoot('helm.json')) || (await findProjectRoot('package.json'));
 
-export function uninstallChart(projectRoot, repoName, chartName) {
-  Promise.all([
-    fs.remove(`${projectRoot}/k8/bases/${chartName}`),
-    fs.remove(`${projectRoot}/helm_charts/${chartName}`),
-    removeChartFromHelmJSON(projectRoot, repoName, chartName),
-    removeChartFromBaseKustomize(projectRoot, chartName),
+  console.log(process.argv);
+  if (!(await fs.pathExists(`${projectRoot}/helm.json`))) {
+    Console.error('helm.json does not exists in this project. Did you run $ helm init first?');
+  } else if (!process.argv[3]) {
+    Console.error('uninstall command needs a package name. Example: helman uninstall stable/nginx-ingress');
+  }
+
+  const deletedCharts = await Promise.all(process.argv.slice(3).map((arg) => uninstallChart(projectRoot, arg), []));
+
+  return await Promise.all([
+    removeChartsFromHelmJSON(projectRoot, deletedCharts),
+    removeChartsFromBaseKustomize(projectRoot, deletedCharts),
   ]);
 }
 
-export function removeChartFromHelmJSON(projectRoot, repoName, chartName) {}
+export async function uninstallChart(projectRoot, fullChartName) {
+  let [repo, name] = fullChartName.split('/');
 
-export function removeChartFromBaseKustomize(projectRoot, chartName) {}
+  await Promise.all([fs.remove(`${projectRoot}/k8s/bases/${name}`), fs.remove(`${projectRoot}/helm_charts/${name}`)]);
+
+  return { repo, name };
+}
+
+export async function removeChartsFromHelmJSON(projectRoot, deletedCharts) {
+  let helmJSON = JSON.parse((await fs.readFile(`${projectRoot}/helm.json`)).toString());
+
+  deletedCharts.forEach((deletedChart) => {
+    delete helmJSON.dependencies[`${deletedChart.repo}/${deletedChart.name}`];
+  });
+
+  return await fs.writeFile(`${projectRoot}/helm.json`, JSON.stringify(helmJSON, null, 2));
+}
+
+export async function removeChartsFromBaseKustomize(projectRoot, deletedCharts) {
+  let baseKustomizationYAML = YAML.parse((await fs.readFile(`${projectRoot}/k8s/bases/kustomization.yaml`)).toString());
+  let targetYAML = Object.assign(baseKustomizationYAML, {
+    bases: baseKustomizationYAML.bases.filter((base) => {
+      return !deletedCharts.find((deletedChart) => base.startsWith(deletedChart.name));
+    }),
+  });
+
+  return await fs.writeFile(`${projectRoot}/k8s/bases/kustomization.yaml`, YAML.stringify(targetYAML));
+}
--- test/commands/uninstall-test.js
+++ test/commands/uninstall-test.js
@@ -2,20 +2,148 @@ import test from 'ava';
 import fs from 'fs-extra';
 import { promisify } from 'util';
 import { exec } from 'child_process';
+import YAML from 'yaml';
 
 const shell = promisify(exec);
 const CWD = process.cwd();
 
 test.beforeEach(async () => {
-  await fs.remove('testapp');
+  await Promise.all([fs.remove(`${CWD}/helm.json`), fs.remove(`${CWD}/helm_charts`), fs.remove(`${CWD}/k8s`)]);
 });
 
 test.afterEach.always(async () => {
-  await fs.remove('testapp');
+  await Promise.all([fs.remove(`${CWD}/helm.json`), fs.remove(`${CWD}/helm_charts`), fs.remove(`${CWD}/k8s`)]);
 });
 
-test('$ helman uninstall without $chartName shows error', async (t) => {});
+test.serial('$ helman uninstall without helm.json shows error', async (t) => {
+  const proc = await t.throwsAsync(() => shell(`node ${CWD}/cli.js uninstall jetstack/cert-manager`));
 
-test('$ helman uninstall $chartName works', async (t) => {});
+  t.true(proc.stderr.includes('helman helm.json does not exists in this project. Did you run $ helm init first?'));
+});
 
-test('$ helman uninstall $chartName $secondChartName $thirdChartName works', async (t) => {});
+test.serial('$ helman uninstall without $chartName shows error', async (t) => {
+  await shell('helm repo add jetstack https://charts.jetstack.io');
+  await fs.writeFile(`${CWD}/helm.json`, JSON.stringify({
+    name: 'helman',
+    dependencies: {
+      'stable/docker-registry': '1.9.2',
+      'stable/nginx-ingress': '0.8.5',
+      'jetstack/cert-manager': '0.12.0'
+    }
+  }));
+
+  const proc = await t.throwsAsync(() => shell(`node ${CWD}/cli.js uninstall`));
+
+  t.true(
+    proc.stderr.includes('helman uninstall command needs a package name. Example: helman uninstall stable/nginx-ingress')
+  );
+});
+
+test.serial('$ helman uninstall $chartName works', async (t) => {
+  await shell('helm repo add jetstack https://charts.jetstack.io');
+  await fs.writeFile(`${CWD}/helm.json`, JSON.stringify({
+    name: 'helman',
+    dependencies: {
+      'stable/nginx-ingress': '0.8.5',
+      'jetstack/cert-manager': '0.12.0'
+    }
+  }));
+  await shell(`node ${CWD}/cli.js install`);
+
+  t.deepEqual(YAML.parse((await fs.readFile(`${CWD}/k8s/bases/kustomization.yaml`)).toString()).bases, [
+    'nginx-ingress',
+    'cert-manager'
+  ]);
+  t.deepEqual(
+    await Promise.all([
+      fs.exists(`${CWD}/helm_charts/cert-manager`),
+      fs.exists(`${CWD}/helm_charts/nginx-ingress`),
+      fs.exists(`${CWD}/k8s/bases/cert-manager`),
+      fs.exists(`${CWD}/k8s/bases/nginx-ingress`),
+    ]),
+    [true, true, true, true]
+  );
+
+  await shell(`node ${CWD}/cli.js uninstall jetstack/cert-manager`);
+
+  const newHelmJSON = JSON.parse((await fs.readFile(`${CWD}/helm.json`)).toString());
+
+  t.deepEqual(newHelmJSON.dependencies, { 'stable/nginx-ingress': '0.8.5' });
+  t.deepEqual(YAML.parse((await fs.readFile(`${CWD}/k8s/bases/kustomization.yaml`)).toString()).bases, [
+    'nginx-ingress'
+  ]);
+  t.deepEqual(
+    await Promise.all([
+      fs.exists(`${CWD}/helm_charts/cert-manager`),
+      fs.exists(`${CWD}/helm_charts/nginx-ingress`),
+      fs.exists(`${CWD}/k8s/bases/cert-manager`),
+      fs.exists(`${CWD}/k8s/bases/nginx-ingress`),
+    ]),
+    [false, true, false, true]
+  );
+
+  await shell(`node ${CWD}/cli.js uninstall stable/nginx-ingress`);
+
+  t.deepEqual(JSON.parse((await fs.readFile(`${CWD}/helm.json`)).toString()).dependencies, {});
+  t.deepEqual(YAML.parse((await fs.readFile(`${CWD}/k8s/bases/kustomization.yaml`)).toString()).bases, [
+  ]);
+  t.deepEqual(
+    await Promise.all([
+      fs.exists(`${CWD}/helm_charts/cert-manager`),
+      fs.exists(`${CWD}/helm_charts/nginx-ingress`),
+      fs.exists(`${CWD}/k8s/bases/cert-manager`),
+      fs.exists(`${CWD}/k8s/bases/nginx-ingress`),
+    ]),
+    [false, false, false, false]
+  );
+});
+
+test.serial('$ helman uninstall $chartName $secondChartName $thirdChartName works', async (t) => {
+  await shell('helm repo add jetstack https://charts.jetstack.io');
+  await fs.writeFile(`${CWD}/helm.json`, JSON.stringify({
+    name: 'helman',
+    dependencies: {
+      'stable/docker-registry': '1.9.2',
+      'stable/nginx-ingress': '0.8.5',
+      'jetstack/cert-manager': '0.12.0'
+    }
+  }));
+  await shell(`node ${CWD}/cli.js install`);
+
+  t.deepEqual(YAML.parse((await fs.readFile(`${CWD}/k8s/bases/kustomization.yaml`)).toString()).bases, [
+    'docker-registry',
+    'nginx-ingress',
+    'cert-manager'
+  ]);
+  t.deepEqual(
+    await Promise.all([
+      fs.exists(`${CWD}/helm_charts/docker-registry`),
+      fs.exists(`${CWD}/helm_charts/cert-manager`),
+      fs.exists(`${CWD}/helm_charts/nginx-ingress`),
+      fs.exists(`${CWD}/k8s/bases/docker-registry`),
+      fs.exists(`${CWD}/k8s/bases/cert-manager`),
+      fs.exists(`${CWD}/k8s/bases/nginx-ingress`),
+    ]),
+    [true, true, true, true, true, true]
+  );
+
+  await shell(`node ${CWD}/cli.js uninstall stable/nginx-ingress jetstack/cert-manager`);
+
+  t.deepEqual(JSON.parse((await fs.readFile(`${CWD}/helm.json`)).toString()).dependencies, {
+    'stable/docker-registry': '1.9.2'
+  });
+  t.deepEqual(YAML.parse((await fs.readFile(`${CWD}/k8s/bases/kustomization.yaml`)).toString()).bases, [
+    'docker-registry'
+  ]);
+  t.deepEqual(
+    await Promise.all([
+      fs.exists(`${CWD}/helm_charts/docker-registry`),
+      fs.exists(`${CWD}/helm_charts/cert-manager`),
+      fs.exists(`${CWD}/helm_charts/nginx-ingress`),
+      fs.exists(`${CWD}/k8s/bases/docker-registry`),
+      fs.exists(`${CWD}/k8s/bases/cert-manager`),
+      fs.exists(`${CWD}/k8s/bases/nginx-ingress`),
+    ]),
+    [true, false, false, true, false, false]
+  );
+});
