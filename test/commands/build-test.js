a547c03217e75a4c67e5b9af7763bf302719ca22
Izel Nakri <contact@izelnakri.com>
2020-04-03 04:41:05 +0200

    v0.9.0 MVP release, all functionality is on tests

--- test/commands/build-test.js
+++ test/commands/build-test.js
@@ -15,7 +15,6 @@ test.afterEach.always(async () => {
   await Promise.all([fs.remove(`${CWD}/helm.json`), fs.remove(`${CWD}/helm_charts`), fs.remove(`${CWD}/k8s`)]);
 });
 
-// NOTE: Values.yaml
 test.serial('$ helman build without helm.json shows error', async (t) => {
   const proc = await t.throwsAsync(() => shell(`node ${CWD}/cli.js build`));
 
@@ -199,5 +198,140 @@ test.serial('$ helman build builds all the charts to correct places', async (t) 
   t.deepEqual(
     YAML.parseAllDocuments((await fs.readFile(`${CWD}/k8s/bases/nginx-ingress/helm.yaml`)).toString()).length,
     5
+  );
+});
+
+test.serial(
+  `$ helman build $chartName and $ helman build $chartName $chartNameTwo takes into account custom values.yaml`,
+  async (t) => {
+    await shell('helm repo add jetstack https://charts.jetstack.io');
+    await fs.writeFile(
+      `${CWD}/helm.json`,
+      JSON.stringify({
+        name: 'helman',
+        dependencies: {
+          'stable/docker-registry': '1.9.2',
+          'stable/nginx-ingress': '0.8.5',
+          'jetstack/cert-manager': '0.12.0',
+        },
+      })
+    );
+    await shell(`node ${CWD}/cli.js install`);
+
+    let certManagerYAML = YAML.parseAllDocuments(
+      (await fs.readFile(`${CWD}/k8s/bases/cert-manager/helm.yaml`)).toString()
+    );
+    let nginxIngressYAML = YAML.parseAllDocuments(
+      (await fs.readFile(`${CWD}/k8s/bases/nginx-ingress/helm.yaml`)).toString()
+    );
+
+    t.deepEqual(
+      YAML.parseAllDocuments((await fs.readFile(`${CWD}/k8s/bases/docker-registry/helm.yaml`)).toString()).length,
+      4
+    );
+    t.deepEqual(certManagerYAML.length, 34);
+    t.true(!certManagerYAML.toString().includes('kubey-test'));
+    t.deepEqual(nginxIngressYAML.length, 5);
+    t.true(!nginxIngressYAML.toString().includes('testy-backend'));
+
+    await Promise.all([
+      fs.writeFile(
+        `${CWD}/k8s/bases/cert-manager/values.yaml`,
+        YAML.stringify({ global: { leaderElection: { namespace: 'kubey-test' } } })
+      ),
+      fs.writeFile(
+        `${CWD}/k8s/bases/nginx-ingress/values.yaml`,
+        YAML.stringify({ defaultBackend: { name: 'testy-backend' } })
+      ),
+    ]);
+    await shell(`node ${CWD}/cli.js build cert-manager`);
+
+    let newCertManagerYAML = YAML.parseAllDocuments(
+      (await fs.readFile(`${CWD}/k8s/bases/cert-manager/helm.yaml`)).toString()
+    );
+    let newNginxIngressYAML = YAML.parseAllDocuments(
+      (await fs.readFile(`${CWD}/k8s/bases/nginx-ingress/helm.yaml`)).toString()
+    );
+
+    t.true(newCertManagerYAML.toString().includes('kubey-test'));
+    t.true(!newNginxIngressYAML.toString().includes('testy-backend'));
+
+    await fs.writeFile(
+      `${CWD}/k8s/bases/cert-manager/values.yaml`,
+      YAML.stringify({ global: { leaderElection: { namespace: 'zing-test' } } })
+    );
+    await shell(`node ${CWD}/cli.js build jetstack/cert-manager nginx-ingress`);
+
+    let lastCertManagerYAML = YAML.parseAllDocuments(
+      (await fs.readFile(`${CWD}/k8s/bases/cert-manager/helm.yaml`)).toString()
+    );
+    let lastNginxIngressYAML = YAML.parseAllDocuments(
+      (await fs.readFile(`${CWD}/k8s/bases/nginx-ingress/helm.yaml`)).toString()
+    );
+
+    t.true(lastCertManagerYAML.toString().includes('zing-test'));
+    t.true(lastNginxIngressYAML.toString().includes('testy-backend'));
+  }
+);
+
+test.serial(`$ helman build takes into account custom values.yaml`, async (t) => {
+  await shell('helm repo add jetstack https://charts.jetstack.io');
+  await fs.writeFile(
+    `${CWD}/helm.json`,
+    JSON.stringify({
+      name: 'helman',
+      dependencies: {
+        'stable/docker-registry': '1.9.2',
+        'stable/nginx-ingress': '0.8.5',
+        'jetstack/cert-manager': '0.12.0',
+      },
+    })
+  );
+  await shell(`node ${CWD}/cli.js install`);
+
+  let certManagerYAML = YAML.parseAllDocuments(
+    (await fs.readFile(`${CWD}/k8s/bases/cert-manager/helm.yaml`)).toString()
+  );
+  let nginxIngressYAML = YAML.parseAllDocuments(
+    (await fs.readFile(`${CWD}/k8s/bases/nginx-ingress/helm.yaml`)).toString()
+  );
+
+  t.deepEqual(
+    YAML.parseAllDocuments((await fs.readFile(`${CWD}/k8s/bases/docker-registry/helm.yaml`)).toString()).length,
+    4
+  );
+  t.deepEqual(certManagerYAML.length, 34);
+  t.true(!certManagerYAML.toString().includes('kubey-test'));
+  t.deepEqual(nginxIngressYAML.length, 5);
+  t.true(!nginxIngressYAML.toString().includes('testy-backend'));
+  t.deepEqual(
+    YAML.parseAllDocuments((await fs.readFile(`${CWD}/k8s/bases/docker-registry/helm.yaml`)).toString()).length,
+    4
+  );
+
+  await Promise.all([
+    fs.writeFile(
+      `${CWD}/k8s/bases/cert-manager/values.yaml`,
+      YAML.stringify({ global: { leaderElection: { namespace: 'kubey-test' } } })
+    ),
+    fs.writeFile(
+      `${CWD}/k8s/bases/nginx-ingress/values.yaml`,
+      YAML.stringify({ defaultBackend: { name: 'testy-backend' } })
+    ),
+  ]);
+  await shell(`node ${CWD}/cli.js build`);
+
+  let newCertManagerYAML = YAML.parseAllDocuments(
+    (await fs.readFile(`${CWD}/k8s/bases/cert-manager/helm.yaml`)).toString()
+  );
+  let newNginxIngressYAML = YAML.parseAllDocuments(
+    (await fs.readFile(`${CWD}/k8s/bases/nginx-ingress/helm.yaml`)).toString()
+  );
+
+  t.true(newCertManagerYAML.toString().includes('kubey-test'));
+  t.true(newNginxIngressYAML.toString().includes('testy-backend'));
+  t.deepEqual(
+    YAML.parseAllDocuments((await fs.readFile(`${CWD}/k8s/bases/docker-registry/helm.yaml`)).toString()).length,
+    4
   );
 });
